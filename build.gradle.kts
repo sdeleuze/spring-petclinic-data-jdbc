eed9a84724fca05dd8d01001857cd4fb34c87a16
Sébastien Deleuze <sdeleuze@vmware.com>
2023-09-21 16:13:17 +0200

    Add CRaC support

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,14 @@
+import org.springframework.boot.gradle.tasks.bundling.BootBuildImage
+
 plugins {
 	java
-	id("org.springframework.boot") version "3.1.4"
+	id("org.springframework.boot") version "3.2.0-M3"
+	id("org.springframework.boot.aot") version "3.2.0-M3"
 	id("io.spring.dependency-management") version "1.1.3"
 	id("org.graalvm.buildtools.native") version "0.9.27"
 }
+
+ext["spring-framework.version"] = "6.1.0-SNAPSHOT"
 
 group = "com.example"
 version = "0.0.1-SNAPSHOT"
@@ -14,9 +19,12 @@ java {
 
 repositories {
 	mavenCentral()
+	maven { url = uri("https://repo.spring.io/milestone") }
+	maven { url = uri("https://repo.spring.io/snapshot") }
 }
 
 dependencies {
+	implementation("org.crac:crac:1.4.0")
 	implementation("org.springframework.boot:spring-boot-starter-actuator")
 	implementation("org.springframework.boot:spring-boot-starter-cache")
 	implementation("org.springframework.boot:spring-boot-starter-data-jdbc")
@@ -32,4 +40,13 @@ dependencies {
 
 tasks.withType<Test> {
 	useJUnitPlatform()
+}
+
+tasks.named<BootBuildImage>("bootBuildImage") {
+	builder = "dashaun/builder:tiny"
+environment.set(mapOf(
+	"BP_NATIVE_IMAGE" to "true",
+//		"BPE_DELIM_JAVA_TOOL_OPTIONS" to " ",
+//		"BPE_APPEND_JAVA_TOOL_OPTIONS" to "-Dspring.aot.enabled=true"
+))
 }
---
+++ checkpoint.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+set -e
+
+case $(uname -m) in
+    arm64)   url="https://cdn.azul.com/zulu/bin/zulu17.44.55-ca-crac-jdk17.0.8.1-linux_aarch64.tar.gz" ;;
+    *)       url="https://cdn.azul.com/zulu/bin/zulu17.44.55-ca-crac-jdk17.0.8.1-linux_x64.tar.gz" ;;
+esac
+
+echo "Using CRaC enabled JDK $url"
+
+./gradlew build -x test
+docker build --no-cache -t sdeleuze/spring-petclinic:builder --build-arg CRAC_JDK_URL=$url .
+docker run -d --cap-add CHECKPOINT_RESTORE --cap-add SYS_ADMIN --cap-add SYS_PTRACE --rm --name=spring-petclinic --ulimit nofile=1024 -p 8080:8080 -v $(pwd)/target:/opt/mnt sdeleuze/spring-petclinic:builder
+echo "Please wait during creating the checkpoint..."
+sleep 20
+docker commit --change='ENTRYPOINT ["/opt/app/entrypoint.sh"]' $(docker ps -qf "name=spring-petclinic") sdeleuze/spring-petclinic:checkpoint
+docker kill $(docker ps -qf "name=spring-petclinic")
