087b983a4129f9eebb00dda14ba3a953bac86771
Sébastien Deleuze <sdeleuze@vmware.com>
2023-09-25 16:48:16 +0200

    WIP

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,11 @@
 plugins {
 	java
-	id("org.springframework.boot") version "3.1.4"
+	id("org.springframework.boot") version "3.2.0-M3"
 	id("io.spring.dependency-management") version "1.1.3"
 }
+
+// Used to get the restoration time logged at Spring Framework level
+ext["spring-framework.version"] = "6.1.0-SNAPSHOT"
 
 group = "com.example"
 version = "1.0.0-SNAPSHOT"
@@ -25,6 +28,7 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-validation")
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("com.github.ben-manes.caffeine:caffeine")
+	implementation("org.crac:crac:1.4.0")
 	compileOnly("com.google.code.findbugs:jsr305:3.0.2")
 	//runtimeOnly("org.postgresql:postgresql")
 	runtimeOnly("com.h2database:h2")
