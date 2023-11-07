dba6078329005cb0457eaada00e4194a4da6c861
Sébastien Deleuze <sdeleuze@vmware.com>
2023-11-07 12:23:47 +0100

    Upgrade Spring Boot to 3.2.0-RC2

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,7 @@ import org.springframework.boot.gradle.tasks.bundling.BootBuildImage
 
 plugins {
 	java
-	id("org.springframework.boot") version "3.2.0-RC1"
+	id("org.springframework.boot") version "3.2.0-RC2"
 	id("io.spring.dependency-management") version "1.1.3"
 }
 
