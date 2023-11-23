1883e933222c517d14d49d0281c56d4999cf502b
Sébastien Deleuze <sdeleuze@vmware.com>
2023-11-23 17:50:24 +0100

    Upgrade to Spring Boot 3.2.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,12 +2,10 @@ import org.springframework.boot.gradle.tasks.bundling.BootBuildImage
 
 plugins {
 	java
-	id("org.springframework.boot") version "3.2.0-SNAPSHOT"
-	id("org.springframework.boot.aot") version "3.2.0-SNAPSHOT"
+	id("org.springframework.boot") version "3.2.0"
+	id("org.springframework.boot.aot") version "3.2.0"
 	id("io.spring.dependency-management") version "1.1.3"
 }
-
-ext["spring-framework.version"] = "6.1.0-SNAPSHOT"
 
 group = "com.example"
 version = "1.0.0-SNAPSHOT"
@@ -17,10 +15,7 @@ java {
 }
 
 repositories {
-	mavenLocal()
 	mavenCentral()
-	maven { url = uri("https://repo.spring.io/milestone") }
-	maven { url = uri("https://repo.spring.io/snapshot") }
 }
 
 dependencies {
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,9 +1,1 @@
-pluginManagement {
-    repositories {
-        mavenLocal()
-        maven { url = uri("https://repo.spring.io/milestone") }
-        maven { url = uri("https://repo.spring.io/snapshot") }
-        gradlePluginPortal()
-    }
-}
 rootProject.name = "spring-petclinic"
