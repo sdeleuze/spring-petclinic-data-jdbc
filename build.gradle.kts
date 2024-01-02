af2fd822a129c01616bd7112773b43d8e50d1063
Sébastien Deleuze <sebastien.deleuze@broadcom.com>
2024-01-02 22:03:07 +0100

    Upgrade to Spring Boot 3.2.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,7 @@ import org.springframework.boot.gradle.tasks.bundling.BootBuildImage
 
 plugins {
 	java
-	id("org.springframework.boot") version "3.2.0"
+	id("org.springframework.boot") version "3.2.1"
 	id("io.spring.dependency-management") version "1.1.3"
 }
 
